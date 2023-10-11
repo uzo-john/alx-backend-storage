755a10415c68143613f3c8807acd1506567d6eaa
uzo-john <johnpauluzowuru2018@gmail.com>
2023-10-11 15:06:45 -0700

    tasks don

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
